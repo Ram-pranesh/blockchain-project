ea7b843bbc53a81a32b6cc0586c13bc8b6d623e8
Ram-pranesh <rampranesh07@gmail.com>
2025-08-08 20:15:44 +0530

    bug fixes

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ require('dotenv').config()
 
 const express = require("express");
 const mongoose = require("mongoose");
-const user = require("./backend/models/userModels");
-const router = require("./backend/routes/UserRoute");
+const user = require("./User-Registration/backend/models/userModels");
+const router = require("./User-Registration/backend/routes/UserRoute");
 const app = express();
 app.use(express.json());
 app.use("/api/v1/users",router);
 
-const PORT = process.env.port;
-const MONGO_URI = process.env.MONGO_URI_ConnectionString
+const PORT=process.env.PORT;
+const MONGO_URI=process.env.MONGO_URI_ConnectionString
 
 mongoose.connect(MONGO_URI)
 .then(()=> {
@@ -17,5 +17,6 @@ mongoose.connect(MONGO_URI)
     app.listen(PORT,() => {console.log(`app is listening to the port : ${PORT}`);})
 })
 .catch((error) => {
-    console.log(error);
+    console.error('DB connection failed : ' ,error);
+    process.exit(1)
 })
